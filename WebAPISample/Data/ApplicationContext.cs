2d6f4959f2b46e256ae0474a508ba1a491fe92ae
Timsy Magnus <tmherron09@gmail.com>
2020-08-31 14:55:54 -0500

    Added Seed Data to ApplicationContext

--- WebAPISample/Data/ApplicationContext.cs
+++ WebAPISample/Data/ApplicationContext.cs
@@ -15,7 +15,27 @@ namespace WebAPISample.Data
         {
             base.OnModelCreating(modelBuilder);
             // Seed data - needs migration
-           
+
+            modelBuilder.Entity<Movie>()
+             .HasData(
+                new Movie { MovieId = 1, Title = "The Departed", Genre = "Drama", Director = "Martin Scorsese" },
+                new Movie { MovieId = 2, Title = "The Dark Knight", Genre = "Drama", Director = "Christopher Nolan" },
+                new Movie { MovieId = 3, Title = "Inception", Genre = "Drama", Director = "Christopher Nolan" },
+                new Movie { MovieId = 4, Title = "Pineapple Express", Genre = "Comedy", Director = "David Gordon Green" },
+                new Movie { MovieId = 5, Title = "Die Hard", Genre = "Action", Director = "John McTiernan" },
+                new Movie { MovieId = 6, Title = "Audition", Genre = "Suspense", Director = "Takashii Miike" },
+                new Movie { MovieId = 7, Title = "Happieness of the Katakuri", Genre = "Musical", Director = "Takashii Miike" },
+                new Movie { MovieId = 8, Title = "Red Green: Duct Tape Forever", Genre = "Comedy", Director = "Eric Till" },
+                new Movie { MovieId = 9, Title = "Send Me No Flowers", Genre = "Comedy", Director = "Norman Jewison" },
+                new Movie { MovieId = 10, Title = "Captain America: The First Avenger", Genre = "Action", Director = "Joe Johnston" },
+                new Movie { MovieId = 11, Title = "Clue", Genre = "Comedy", Director = "Johnathan Lynn" },
+                new Movie { MovieId = 12, Title = "The Grass is Greener", Genre = "Comedy", Director = "Stanley Donen" },
+                new Movie { MovieId = 13, Title = "Operation Petticoat", Genre = "Comedy", Director = "JBlake Edwards" },
+                new Movie { MovieId = 14, Title = "Step Brothers", Genre = "Comedy", Director = "Adam Mckay" },
+                new Movie { MovieId = 15, Title = "Fallen Angels", Genre = "Comedy", Director = "Kar-Wai Wong" }
+             );
+
+
         }
 
         public DbSet<Movie> Movies { get; set; }
