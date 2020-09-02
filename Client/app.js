c9bafd644fcf773f8e490d19dfc7afd88679d8e2
chouac1 <chouac@live.com>
2020-09-02 15:37:48 -0500

    Styles added into project. Implement cards for movie list for a better user interface. Currently working on completing search bar.

--- Client/app.js
+++ Client/app.js
@@ -50,25 +50,40 @@ function getAllMovies(){
         },
     })
     .then(function(data){
-        $.each(data, function(index, value){
-            $('#MovieTable').append(
+        $.each(data, function(index, value){                
+            $("#movieCardContainer").append(
                 `
-                <tr>
-                    <td>${value.title}</td>
-                    <td>${value.genre}</td>
-                    <td>${value.director}</td>
-                </tr>
+                <div id="${value.movieId}" class="col-md-6">
+                    <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                        
+                    <div class="col-8 p-4 d-flex flex-column position-static">
+                        <strong class="d-inline-block mb-2 text-primary">${value.genre}</strong>
+                        <h3 class="mb-0">${value.title}</h3>
+                            
+                        <div class="mb-1 text-muted">${value.director}</div>
+
+                        <p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+                        <button onclick="editmovie(${value.movieId})" class="btn-primary">View details</a>
+                        </div>
+                        
+                        <div class="col-4 d-none d-lg-block">
+                        <img src="" class="img-fluid" alt="movie poster for ${value.title}">
+                        </div>
+                    </div>
+                </div>
                 `
             )
-        })
-    })
-}
+        }
+        )})
+    }
+                
+    
 
 function getMovieById(id) {
     return $.ajax({
         type: 'GET',
         dataType: 'json',
-        url: 'http://71.11.153.207:9000/api/movie/' + id,
+        url: 'https://localhost:44325/api/movie/' + id,
         success: function () {
             console.log("GET id Success.");
         },
@@ -91,7 +106,7 @@ async function processEditMovieForm(e) {
 
     e.preventDefault();
     await jQuery.ajax({
-        url: 'http://71.11.153.207:9000/api/movie',
+        url: 'https://localhost:44325/api/movie',
             type: 'PUT',
             contentType: 'application/json',
             data: outgoing,
@@ -105,3 +120,4 @@ async function processEditMovieForm(e) {
     })
     getAllMovies();
 }
+
