0e52f805a9dae0dea2dace234ed84a0b20f631bd
Timsy Magnus <tmherron09@gmail.com>
2020-09-02 09:00:10 -0500

    Updated Put method with validation for empty check

--- WebAPISample/Controllers/MovieController.cs
+++ WebAPISample/Controllers/MovieController.cs
@@ -59,12 +59,16 @@ namespace WebAPISample.Controllers
         public IActionResult Put([FromBody] Movie updatedMovie)
         {
             // Update movie in db logic
-            Movie movie = _context.Movies.Where(m  =>  m.MovieId == updatedMovie.MovieId).FirstOrDefault();
-            movie.Title = updatedMovie.Title;
-            movie.Genre = updatedMovie.Genre;
-            movie.Director = updatedMovie.Director;
-            _context.SaveChanges();
+            Movie movieToUpdate = _context.Movies.Where(m  =>  m.MovieId == updatedMovie.MovieId).FirstOrDefault();
 
+            // Validation backup Check that incoming isn't empty or null, if it is, set to original value. Allows sending only one input to change.
+            movieToUpdate.Title = (updatedMovie.Title == "" || updatedMovie.Title == null) ? movieToUpdate.Title : updatedMovie.Title;
+
+            movieToUpdate.Director = (updatedMovie.Director == "" || updatedMovie.Director == null) ? movieToUpdate.Director : updatedMovie.Director;
+
+            movieToUpdate.Genre = (updatedMovie.Genre == "" || updatedMovie.Genre == null) ? movieToUpdate.Genre : updatedMovie.Genre;
+
+            _context.SaveChanges();
 
             return Ok();
         }
